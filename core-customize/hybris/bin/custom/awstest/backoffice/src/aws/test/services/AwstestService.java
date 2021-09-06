c281d004e7090c34d4854c2c23b4bad2f85970d1
stacy425 <yanqian.shen@sap.com>
2021-09-06 12:33:15 +0800

    testcolumntypechange

---
+++ core-customize/hybris/bin/custom/awstest/backoffice/src/aws/test/services/AwstestService.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved
+ */
+package aws.test.services;
+
+/**
+ * Hello World AwstestService
+ */
+public class AwstestService
+{
+	public String getHello()
+	{
+		return "Hello";
+	}
+}
---
+++ core-customize/hybris/bin/custom/awstest/src/aws/test/constants/AwstestConstants.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved
+ */
+package aws.test.constants;
+
+/**
+ * Global class for all Ybackoffice constants. You can add global constants for your extension into this class.
+ */
+public final class AwstestConstants extends GeneratedAwstestConstants
+{
+	public static final String EXTENSIONNAME = "awstest";
+
+	private AwstestConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
